b6796869706fd3e88a83813962f1ce23b0b72ab2
Anton <antongorynya1@gmail.com>
2017-11-10 17:52:56 +0300

    add logging

--- image_resize.py
+++ image_resize.py
@@ -1,5 +1,14 @@
 from PIL import Image
 import argparse
+import logging
+import os
+
+
+VERBOSITY_TO_LOGGING_LEVELS = {
+    0: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG,
+}
 
 
 def create_pic_name(input_image_name, width, hight):
@@ -10,59 +19,77 @@ def create_pic_name(input_image_name, width, hight):
 
 
 def set_size(original_width, original_hight,
-             target_width=None, target_hight=None, target_sacle=None):
-    if target_hight and target_width:
-        if original_width/target_width != original_hight/target_hight:
-            print('warning! Scale mismatch. x_scale {}, y_scale {}'
-                  .format(original_width/target_width,
-                          original_hight/target_hight))
+             target_width=None, target_hight=None, target_scale=None):
+    logging.debug('\noriginal_width={}\n'
+                  'original_hight={}\n'
+                  'target_width={}\n'
+                  'target_hight={}\n'
+                  'target_scale={}'
+                  .format(original_width, original_hight,
+                          target_width, target_hight, target_scale))
+    if target_hight and target_width and not target_scale:
+        x_scale = original_width/target_width
+        y_scale = original_hight/target_hight
+        rel_tol = 1e-09
+        if abs(x_scale-y_scale) > max(rel_tol*max(x_scale, y_scale), 0.0):
+            logging.info('\nwarning! Scale mismatch. x_scale {}, y_scale {}'
+                         .format(x_scale, y_scale))
         width = target_width
-        hight = target_width
+        hight = target_hight
         return width, hight
-    elif target_sacle and not (target_hight or target_width):
-        width = int((float(original_width) * float(target_sacle)))
-        hight = int((float(original_hight) * float(target_sacle)))
+    elif target_scale and not (target_hight or target_width):
+        width = int((float(original_width) * float(target_scale)))
+        hight = int((float(original_hight) * float(target_scale)))
         return width, hight
-    elif target_hight:
+    elif target_hight and not target_width:
         scale = (target_hight / float(original_hight))
         width = int((float(original_width) * float(scale)))
         hight = target_hight
         return width, hight
-    elif target_width:
+    elif target_width and not target_hight:
         scale = (target_width / float(original_width))
         hight = int((float(original_hight) * float(scale)))
         width = target_width
         return width, hight
     else:
-        print("wrong paramters")
+        logging.info('wrong paramters')
 
 
-def resize_image(path_to_original, out_img,
-                 target_width=None, target_hight=None, target_sacle=None):
+def resize_image(path_to_original, out_dir=None,
+                 target_width=None, target_hight=None, target_scale=None):
     img = Image.open(path_to_original)
-    original_width = img.size[0]
-    original_hight = img.size[1]
-    width, hight = set_size(original_width, original_hight,
-                            target_width, target_hight, target_sacle)
-    img = img.resize((width, hight), Image.ANTIALIAS)
-    if not out_img:
-        out_img = create_pic_name(path_to_original, width, hight)
-    img.save(out_img)
+    original_width, original_hight = img.size
+    try:
+        width, hight = set_size(original_width, original_hight,
+                                target_width, target_hight, target_scale)
+        img = img.resize((width, hight), Image.ANTIALIAS)
+        out_img_name = create_pic_name(path_to_original, width, hight)
+        if not out_dir:
+            out_img = out_img_name
+        else:
+            out_img = os.path.join(out_dir, out_img_name)
+        print(out_img)
+        img.save(out_img)
+    except TypeError:
+        logging.info('image don\'t created')
 
 
 def create_parser():
     parser = argparse.ArgumentParser(description='resize image')
-    parser.add_argument("path", help="path to image")
-    parser.add_argument("-width", type=int, help="input width")
-    parser.add_argument("-hight", type=int, help="input hight")
-    parser.add_argument("-scale", type=float, help="input hight")
-    parser.add_argument("-output",
+    parser.add_argument('path', help='path to image')
+    parser.add_argument('-width', type=int, help='input width')
+    parser.add_argument('-hight', type=int, help='input hight')
+    parser.add_argument('-scale', type=float, help='input hight')
+    parser.add_argument('--verbose', '-v', type=int, default=0)
+    parser.add_argument('-output',
                         default='',
-                        type=str, help="path to output file")
+                        type=str, help='path to output dir')
     return parser
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
+    logging_level = VERBOSITY_TO_LOGGING_LEVELS[args.verbose]
+    logging.basicConfig(level=logging_level)
     resize_image(args.path, args.output, args.width, args.hight, args.scale)
